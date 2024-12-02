b2e839aaf126deb144224ef9fb94f198db703da2
Alex <oleksandrtaranenko239@gmail.com>
2024-12-02 15:59:54 +0200

    add Borrowing model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.core.validators import MinValueValidator
 
@@ -27,3 +28,23 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author}"
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return = models.DateField()
+    actual_return = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="borrowings",
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="borrowings",
+    )
+
+    def __str__(self):
+        return f"{self.user} borrowed {self.book} on {self.borrow_date}"
+
