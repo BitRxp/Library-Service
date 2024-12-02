73e0e0318ce46b1f89f5e2d57a69550bdf33a460
Alex <oleksandrtaranenko239@gmail.com>
2024-12-02 16:08:16 +0200

    add Payment model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.conf import settings
 from django.db import models
 from django.core.validators import MinValueValidator
@@ -46,5 +48,55 @@ class Borrowing(models.Model):
     )
 
     def __str__(self):
-        return f"{self.user} borrowed {self.book} on {self.borrow_date}"
+        return (
+            f"{self.user}"
+            f" borrowed {self.book}"
+            f" on {self.borrow_date}"
+        )
 
+
+class Payment(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "PENDING", "Pending"
+        PAID = "PAID", "Paid"
+
+    class Type(models.TextChoices):
+        PAYMENT = "PAYMENT", "Payment"
+        FINE = "FINE", "Fine"
+
+    status = models.CharField(
+        choices=Status.choices,
+        default=Status.PENDING,
+        max_length=7,
+    )
+    type = models.CharField(
+        choices=Type.choices,
+        default=Type.PAYMENT,
+        max_length=7,
+    )
+    borrowing = models.ForeignKey(
+        Borrowing,
+        on_delete=models.CASCADE,
+        related_name="payments",
+    )
+    Session_url = models.URLField(
+        help_text="URL to Stripe payment session"
+    )
+
+    session_id = models.CharField(
+        max_length=255,
+        help_text="ID of Stripe payment session"
+    )
+    money_to_pay = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=Decimal("0.00"),
+        help_text="Calculated borrowing total price in $USD"
+    )
+
+    def __str__(self):
+        return (
+            f"Payment for Borrowing ID"
+            f" {self.borrowing.id}: {self.status}"
+            f" ({self.type})"
+        )
