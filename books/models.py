974557c5203ab37ec7a4b2f2ea154cd4f586d10f
Alex <oleksandrtaranenko239@gmail.com>
2024-12-03 09:15:25 +0200

    fix daily_fee validator in Book model

--- books/models.py
+++ books/models.py
@@ -24,7 +24,7 @@ class Book(models.Model):
     daily_fee = models.DecimalField(
         max_digits=10,
         decimal_places=2,
-        validators=[MinValueValidator("0.01")],
+        validators=[MinValueValidator(Decimal("0.01"))],
         help_text="Daily fee in $USD"
     )
 
